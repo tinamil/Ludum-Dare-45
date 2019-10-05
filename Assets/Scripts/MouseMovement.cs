898fb08d52e5589abbf0ebddc7a4e978ae958f90
John Pavlik <john.a.pavlik@gmail.com>
2019-10-05 00:58:39 -0500

    Day 1

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
+
+    public int margin = 30;
+
+    private Vector3 middleClickPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(2))
+        {
+            middleClickPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButton(2))
+        {
+            var pixelMotion = -Input.mousePosition + middleClickPosition;
+            transform.Translate(pixelMotion.x * .01f, pixelMotion.y * .01f, 0);
+            middleClickPosition = Input.mousePosition;
+        }
+        else
+        {
+
+            //Do not move camera if mouse leaves screen
+            if (Input.mousePosition.x < 0 || Input.mousePosition.y < 0 || Input.mousePosition.x > Screen.width || Input.mousePosition.y > Screen.height)
+            {
+                return;
+            }
+
+            if (Input.mousePosition.x < margin)
+            {
+                transform.Translate(-Time.deltaTime * 10, 0, 0);
+            }
+            if (Input.mousePosition.y < margin)
+            {
+                transform.Translate(0, -Time.deltaTime * 10, 0);
+            }
+            if (Input.mousePosition.x > (Screen.width - margin))
+            {
+                transform.Translate(Time.deltaTime * 10, 0, 0);
+            }
+            if (Input.mousePosition.y > (Screen.height - margin))
+            {
+                transform.Translate(0, Time.deltaTime * 10, 0);
+            }
+        }
     }
 }
