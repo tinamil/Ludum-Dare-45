9d59f1233bd9e2438624e3d3cabca046b8519e9c
John Pavlik <john.a.pavlik@gmail.com>
2019-10-05 16:55:30 -0500

    Added selection box

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private Vector2 mouseDown;
+    private Vector2 mouseUp;
+    bool released = false;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mouseDown = Input.mousePosition;
+            released = false;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseUp = Input.mousePosition;
+            released = true;
+            var colliders = Physics2D.OverlapAreaAll(Camera.main.ScreenToWorldPoint(mouseDown), Camera.main.ScreenToWorldPoint(mouseUp), LayerMask.GetMask("Selectable"));
+            foreach (var c in colliders)
+            {
+                Debug.Log("Found collider : " + c.name);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (released)
+        {
+            released = false;
+        }
+    }
+}
---
+++ Assets/Scripts/GroundManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class GroundManager : MonoBehaviour
+{
+    Tilemap tiles;
+    Grid grid;
+
+    void Start()
+    {
+        tiles = transform.GetComponentInChildren<Tilemap>();
+        grid = transform.GetComponentInChildren<Grid>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pz = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            pz.z = 0;
+            Vector3Int cellPosition = grid.WorldToCell(pz);
+
+            //tiles.SetTile(cellPosition, null);
+        }
+    }
+}
